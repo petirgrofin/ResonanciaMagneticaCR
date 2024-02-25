49222f8fffb8453c7dbf6f99a2925f1448133994
petirgrofin <felipebolanos026@gmail.com>
2024-02-25 16:15:24 -0600

    Updates

--- assets/MRICanvas-6b06e498.js
+++ assets/MRICanvas-6b06e498.js
@@ -1,4 +1,4 @@
-import{r as Qi,j as ki}from"./index-0d386f3d.js";function Jt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function Ko(a,e){a.prototype=Object.create(e.prototype),a.prototype.constructor=a,a.__proto__=e}/*!
+import{r as Qi,j as ki}from"./index-9417d7bc.js";function Jt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function Ko(a,e){a.prototype=Object.create(e.prototype),a.prototype.constructor=a,a.__proto__=e}/*!
  * GSAP 3.12.5
  * https://gsap.com
  *
